3bb461ba0e9bd6e19c1b244d69d9b29c86e8b646
Dan <danfenton@pm.me>
2025-08-20 09:15:35 +0100

    Refactored

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,54 @@
 -- All language servers are configured in this file
 return {
-	{
-		-- Neovim built in LSP client
-		-- Sends and receives events from Neovim to language servers via language server protocol
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							-- Prevents the vim commands from being highlighted as an error
-							globals = { "vim" },
-						},
-					},
-				},
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
+  {
+    -- Neovim built in LSP client
+    -- Sends and receives events from Neovim to language servers via language server protocol
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Prevents the vim commands from being highlighted as an error
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
 
-			-- keymaps for language server functions
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>oi",
-				vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-			)
-		end,
-	},
+      -- keymaps for language server functions
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>oi", function()
+        vim.lsp.buf.code_action({
+          context = { only = { "source.organizeImports" } },
+          apply = true,
+        })
+      end)
+    end,
+  },
 }
--- lua/plugins/monokai-pro.lua
+++ lua/plugins/monokai-pro.lua
@@ -1,14 +1,24 @@
 -- Color theme for Neovim
 return {
-  'loctvl842/monokai-pro.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function ()
-    require("monokai-pro").setup({
-      filter = "classic",
-    })
+	"loctvl842/monokai-pro.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		filter = "classic",
+		styles = {
+			comment = { italic = true },
+			keyword = { italic = true }, -- any other keyword
+			type = { italic = true }, -- (preferred) int, long, char, etc
+			storageclass = { italic = true }, -- static, register, volatile, etc
+			structure = { italic = true }, -- struct, union, enum, etc
+			parameter = { italic = true }, -- parameter pass in function
+			annotation = { italic = true },
+		},
+	},
+	config = function(_, opts)
+    require("monokai-pro").setup(opts)
 
-    -- Set the color scheme for Neovim
-    vim.cmd.colorscheme("monokai-pro")
-  end
+		-- Set the color scheme for Neovim
+		vim.cmd.colorscheme("monokai-pro")
+	end,
 }
