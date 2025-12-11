b0a4fc52e474e9a448951437ef29c603577e78c0
Dan <danfenton@pm.me>
2025-12-11 21:22:13 +0000

    fixed typo with lua_ls lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,7 +7,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      vim.lsp.config('lua-ls', {
+      vim.lsp.config('lua_ls', {
         capabilities = capabilities,
         settings = {
           Lua = {
