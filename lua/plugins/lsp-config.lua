4e0091e71b9b223e02b40a5e9e8d90bc66e1e00e
b7 <danfenton@pm.me>
2026-01-02 20:17:59 +0000

    added nixd language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,74 +1,79 @@
 -- All language servers are configured in this file
 return {
-	{
-		-- Neovim built in LSP client
-		-- Sends and receives events from Neovim to language servers via language server protocol
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						diagnostics = {
-							-- Prevents the vim commands from being highlighted as an error
-							globals = { "vim" },
-						},
-					},
-				},
-			})
+  {
+    -- Neovim built in LSP client
+    -- Sends and receives events from Neovim to language servers via language server protocol
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Prevents the vim commands from being highlighted as an error
+              globals = { "vim" },
+            },
+          },
+        },
+      })
       vim.lsp.enable("lua_ls")
 
-			vim.lsp.config("pyright", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
       vim.lsp.enable("pyright")
 
-			vim.lsp.config("ts_ls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("nixd", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("nixd")
+
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
       vim.lsp.enable("ts_ls")
 
-			vim.lsp.config("bashls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("bashls", {
+        capabilities = capabilities,
+      })
       vim.lsp.enable("bashls")
 
-			vim.lsp.config("html", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("html", {
+        capabilities = capabilities,
+      })
       vim.lsp.enable("html")
 
-			vim.lsp.config("cssls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("cssls", {
+        capabilities = capabilities,
+      })
       vim.lsp.enable("cssls")
 
       -- Diagnostic settings
-			vim.diagnostic.config({
-				virtual_text = false,
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			})
+      vim.diagnostic.config({
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
 
-			-- keymaps for language server functions
+      -- keymaps for language server functions
       vim.keymap.set('n', 'gl', vim.diagnostic.open_float, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>oi", function()
-				vim.lsp.buf.code_action({
-					context = { only = { "source.organizeImports" } },
-					apply = true,
-				})
-			end)
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>oi", function()
+        vim.lsp.buf.code_action({
+          context = { only = { "source.organizeImports" } },
+          apply = true,
+        })
+      end)
+    end,
+  },
 }
